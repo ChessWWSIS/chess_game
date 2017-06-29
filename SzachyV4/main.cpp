ba87304b6b99495b7c4cd123f5d57029b8dd4f9b
rafurz <rafurz@gmail.com>
2017-06-29 18:38:50 +0200

    Update main.cpp

--- SzachyV4/main.cpp
+++ SzachyV4/main.cpp
@@ -6,12 +6,6 @@
 
 using namespace std;
 
-class Pionek
-{
-    public:
-
-};
-
 int main()
 {
 
@@ -34,7 +28,11 @@ int main()
 
 
     bool czyPoleZaznaczone, graWToku = false;
-    int indeksPionka;
+
+
+    int board [8][8];
+    int nowaPozycja(int x, int y);
+    int czyNastRPionka[16];
 
     while(okno.isOpen())
     {
@@ -49,12 +47,13 @@ int main()
         if (graWToku == false)
         {
             for (int i = 0; i < 8; i++)
-                {
-                    f[i].setTexture(bierki);
-                    f[i].setTextureRect(sf::IntRect(500, 100, 100, 100));
-                    okno.draw(f[i]);
-                    f[i].setPosition(100*i, 100);
-                }
+            {
+                f[i].setTexture(bierki);
+                f[i].setTextureRect(sf::IntRect(500, 100, 100, 100));
+                okno.draw(f[i]);
+                f[i].setPosition(100*i, 100);
+                board[i][6] = i+1;
+            }
 
             for (int i = 8; i < 16; i++)
                 {
@@ -62,88 +61,106 @@ int main()
                     f[i].setTextureRect(sf::IntRect(500, 0, 100, 100));
                     okno.draw(f[i]);
                     f[i].setPosition(100*(i-8), 600);
+                    board[i-8][6] = i+1;
                 }
 
-            for (int i = 16; i < 18; i++)
-                {
-                    f[i].setTexture(bierki);
-                    f[i].setTextureRect(sf::IntRect(0, 100, 100, 100));
-                    okno.draw(f[i]);
-                    f[i].setPosition(0,0);
-                    okno.draw(f[i]);
-                    f[i].setPosition(700,0);
-                }
 
-            for (int i = 18; i < 20; i++)
-                {
-                    f[i].setTexture(bierki);
-                    f[i].setTextureRect(sf::IntRect(0, 0, 100, 100));
-                    okno.draw(f[i]);
-                    f[i].setPosition(0,700);
-                    okno.draw(f[i]);
-                    f[i].setPosition(700,700);
-                }
+                f[16].setTexture(bierki);
+                f[16].setTextureRect(sf::IntRect(0, 100, 100, 100));
+                okno.draw(f[16]);
+                f[16].setPosition(0,0);
+                board[0][0] = 17;
 
-            for (int i = 20; i < 22; i++)
-                {
-                    f[i].setTexture(bierki);
-                    f[i].setTextureRect(sf::IntRect(100, 100, 100, 100));
-                    okno.draw(f[i]);
-                    f[i].setPosition(100,0);
-                    okno.draw(f[i]);
-                    f[i].setPosition(600,0);
-                }
+                f[17].setTexture(bierki);
+                f[17].setTextureRect(sf::IntRect(0, 100, 100, 100));
+                okno.draw(f[17]);
+                f[17].setPosition(700,0);
+                board[0][7] = 18;
 
-            for (int i = 22; i < 24; i++)
-                {
-                    f[i].setTexture(bierki);
-                    f[i].setTextureRect(sf::IntRect(100, 0, 100, 100));
-                    okno.draw(f[i]);
-                    f[i].setPosition(100,700);
-                    okno.draw(f[i]);
-                    f[i].setPosition(600,700);
-                }
+                f[18].setTexture(bierki);
+                f[18].setTextureRect(sf::IntRect(0, 0, 100, 100));
+                okno.draw(f[18]);
+                f[18].setPosition(0,700);
+                board[7][0] = 19;
 
-            for (int i = 24; i < 26; i++)
-                {
-                    f[i].setTexture(bierki);
-                    f[i].setTextureRect(sf::IntRect(200, 100, 100, 100));
-                    okno.draw(f[i]);
-                    f[i].setPosition(200,0);
-                    okno.draw(f[i]);
-                    f[i].setPosition(500,0);
-                }
+                f[19].setTexture(bierki);
+                f[19].setTextureRect(sf::IntRect(0, 0, 100, 100));
+                okno.draw(f[19]);
+                f[19].setPosition(700,700);
+                board[7][7] = 20;
 
-            for (int i = 26; i < 28; i++)
-                {
-                    f[i].setTexture(bierki);
-                    f[i].setTextureRect(sf::IntRect(200, 0, 100, 100));
-                    okno.draw(f[i]);
-                    f[i].setPosition(200,700);
-                    okno.draw(f[i]);
-                    f[i].setPosition(500,700);
-                }
+                f[20].setTexture(bierki);
+                f[20].setTextureRect(sf::IntRect(100, 100, 100, 100));
+                okno.draw(f[20]);
+                f[20].setPosition(100,0);
+                board[0][1] = 21;
+
+                f[21].setTexture(bierki);
+                f[21].setTextureRect(sf::IntRect(100, 100, 100, 100));
+                okno.draw(f[21]);
+                f[21].setPosition(600,0);
+                board[0][6] = 22;
+
+                f[22].setTexture(bierki);
+                f[22].setTextureRect(sf::IntRect(100, 0, 100, 100));
+                okno.draw(f[22]);
+                f[22].setPosition(100,700);
+                board[7][1] = 23;
+
+                f[23].setTexture(bierki);
+                f[23].setTextureRect(sf::IntRect(100, 0, 100, 100));
+                okno.draw(f[23]);
+                f[23].setPosition(600,700);
+                board[7][6] = 24;
+
+                f[24].setTexture(bierki);
+                f[24].setTextureRect(sf::IntRect(200, 100, 100, 100));
+                okno.draw(f[24]);
+                f[24].setPosition(200,0);
+                board[0][2] = 25;
+
+                f[25].setTexture(bierki);
+                f[25].setTextureRect(sf::IntRect(200, 100, 100, 100));
+                okno.draw(f[25]);
+                f[25].setPosition(500,0);
+                board[0][5] = 26;
+
+                f[26].setTexture(bierki);
+                f[26].setTextureRect(sf::IntRect(200, 0, 100, 100));
+                okno.draw(f[26]);
+                f[26].setPosition(200,700);
+                board[7][2] = 27;
+
+                f[27].setTexture(bierki);
+                f[27].setTextureRect(sf::IntRect(200, 0, 100, 100));
+                okno.draw(f[27]);
+                f[27].setPosition(500,700);
+                board[7][5] = 28;
 
 
             f[28].setTexture(bierki);
             f[28].setTextureRect(sf::IntRect(300, 100, 100, 100));
             okno.draw(f[28]);
             f[28].setPosition(300,000);
+            board[0][3] = 29;
 
             f[29].setTexture(bierki);
             f[29].setTextureRect(sf::IntRect(300, 0, 100, 100));
             okno.draw(f[29]);
             f[29].setPosition(300,700);
+            board[7][3] = 30;
 
             f[30].setTexture(bierki);
             f[30].setTextureRect(sf::IntRect(400, 100, 100, 100));
             okno.draw(f[30]);
             f[30].setPosition(400,000);
+            board[0][4] = 31;
 
             f[31].setTexture(bierki);
             f[31].setTextureRect(sf::IntRect(400, 0, 100, 100));
             okno.draw(f[31]);
             f[31].setPosition(400,700);
+            board[7][4] = 32;
 
 
         }
@@ -157,16 +174,137 @@ int main()
             pozycjaMyszy.x = (pozycjaMyszy.x / 100) * 100;
             pozycjaMyszy.y = (pozycjaMyszy.y / 100) * 100;
             czyPoleZaznaczone = true;
-            for (int i = 0; i < 32; i++)
+        }
+        for (int i = 0; i < 32; i++)
             {
-                if (f[i].getPosition().x == sf::Mouse::getPosition( okno ).x && f[i].getPosition().y == sf::Mouse::getPosition( okno ).y)
+                if (f[i].getPosition().x == pozycjaMyszy.x && f[i].getPosition().y == pozycjaMyszy.y)
                 {
-                    //tutaj opcje dla pionkow
+
+                //zaznaczaie mozliwych ruchow
+                    //zaznaczanie dla czarnych pionkow
+                    if (i < 8)
+                    {
+                        if (czyNastRPionka[i] == 0)
+                        {
+                            okno.draw(zaznaczeniePola);
+                            zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y+100);
+                            okno.draw(zaznaczeniePola);
+                            zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y+200);
+                        }
+                        else
+                        {
+                            okno.draw(zaznaczeniePola);
+                            zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y+100);
+                        }
+                    }
+                    //zaznaczanie dla bialych pionkow
+                    else if (i >= 8 && i < 16)
+                    {
+                        if (czyNastRPionka[i] == 0)
+                        {
+                            okno.draw(zaznaczeniePola);
+                            zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y-100);
+                            okno.draw(zaznaczeniePola);
+                            zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y-200);
+                        }
+                        else
+                        {
+                            okno.draw(zaznaczeniePola);
+                            zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y-100);
+                        }
+                    }
+                    //zaznaczanie dla wiez wszystkich wiez
+                    else if (i >= 16 && i < 20)
+                    {
+                        for (int j = -7; j < 8; j++)
+                        {
+                                for (int l = -7; l < 8; l++)
+                                {
+                                    okno.draw(zaznaczeniePola);
+                                    zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y+(l*100));
+                                }
+                            okno.draw(zaznaczeniePola);
+                            zaznaczeniePola.setPosition(f[i].getPosition().x+(j*100),f[i].getPosition().y);
+                        }
+                    }
+                    //zaznaczanie dla wszystkich koni
+                    else if (i >= 20 && i < 24)
+                    {
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x+100,f[i].getPosition().y+200);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x+100,f[i].getPosition().y-200);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x-100,f[i].getPosition().y+200);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x-100,f[i].getPosition().y-200);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x+200,f[i].getPosition().y+100);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x+200,f[i].getPosition().y-100);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x-200,f[i].getPosition().y+100);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x-200,f[i].getPosition().y-100);
+                    }
+                    //znaznaczanie dla wszystkich goncow
+                    else if (i >= 24 && i < 28)
+                    {
+                        for (int j = -7; j < 8; j++)
+                        {
+                                for (int l = -7; l < 8; l++)
+                                {
+                                    okno.draw(zaznaczeniePola);
+                                    zaznaczeniePola.setPosition(f[i].getPosition().x+(l*100),f[i].getPosition().y+(l*100));
+                                }
+                            okno.draw(zaznaczeniePola);
+                            zaznaczeniePola.setPosition(f[i].getPosition().x-(j*100),f[i].getPosition().y+(j*100));
+                        }
+                    }
+                    //zaznaczenie dla hetmanów
+                    else if (i >=28 && i < 30)
+                    {
+                        for (int j = -7; j < 8; j++)
+                        {
+                                for (int l = -7; l < 8; l++)
+                                {
+                                    okno.draw(zaznaczeniePola);
+                                    zaznaczeniePola.setPosition(f[i].getPosition().x+(l*100),f[i].getPosition().y+(l*100));
+                                    okno.draw(zaznaczeniePola);
+                                    zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y+(l*100));
+                                }
+                            okno.draw(zaznaczeniePola);
+                            zaznaczeniePola.setPosition(f[i].getPosition().x-(j*100),f[i].getPosition().y+(j*100));
+                            okno.draw(zaznaczeniePola);
+                            zaznaczeniePola.setPosition(f[i].getPosition().x+(j*100),f[i].getPosition().y);
+                        }
+                    }
+                    //zaznaczanie dla kroli
+                    else if (i >=30 && i <32)
+                    {
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x+100,f[i].getPosition().y+100);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x+100,f[i].getPosition().y-100);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x+100,f[i].getPosition().y);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x-100,f[i].getPosition().y+100);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x-100,f[i].getPosition().y-100);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x-100,f[i].getPosition().y);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y+100);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y-100);
+                        okno.draw(zaznaczeniePola);
+                        zaznaczeniePola.setPosition(f[i].getPosition().x,f[i].getPosition().y);
+                    }
+
+
                 }
             }
-
-
-        }
         if (czyPoleZaznaczone == true)
         {
             okno.draw(zaznaczeniePola);
